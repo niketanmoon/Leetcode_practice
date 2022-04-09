dd2fee5ac4af7a0e65d83b5005ef6ff3d445d099
niketanmoon <niketanmoon@gmail.com>
2022-04-09 15:46:48 +0930

    Added completed version for 66

--- 66 Plus One/plus_one.py
+++ 66 Plus One/plus_one.py
@@ -1,1 +1,11 @@
-plus_one.py
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        my_list = list()
+        # concatenate all the numbers in digit in form of string
+        string_digit = "".join(map(str, digits))
+        final_number = int(string_digit) + 1
+        for i in str(final_number):
+            my_list.append(i)
+        return my_list
+            
+        
